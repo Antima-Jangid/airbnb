5a017d173863e83114f986066031a51d0f5dc531
Antima Jangid <jangidantima270@gmail.com>
2026-02-25 10:08:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const path = require("path");
+const ExpressError  = require("./utils/ExpressError.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(()=>{
+    console.log("connected to db");
+}).catch(err =>{
+    console.log("error cought ", err);
+});
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res)=>{
+    res.send("Hi i am root");
+});
+
+app.use("/listings",listings);
+
+app.use("/listings/:id/reviews",reviews );
+
+app.use((req, res,next)=>{
+    next(new ExpressError(404, "Page not found!"));
+});
+
+app.use((err, req, res, next)=>{
+    let {statusCode=500 , message="Something went wrong"} = err;
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", {message});
+});
+
+const port = 8080;
+app.listen(port, ()=>{
+    console.log("app is listening to port 8080");
+});
